2b7abd72e5c04e8f6c1b4d716e896b90e53db9a8
jhohantellez <jhohan.tellez.p@gmail.com>
2026-05-16 20:13:43 -0500

    The interface of the HTMLs improved

--- app.py
+++ app.py
@@ -6,7 +6,18 @@ app = Flask(__name__)
 #-------------------- HOME --------------------
 @app.route("/")
 def home():
-    return render_template('1stphase/businessdata.html')
+    return render_template('home.html')
+#-------------------- PAGES --------------------
+
+@app.route("/businessdata")
+def business_data():
+    return render_template('businessdata.html')
+
+@app.route("/dataengineering")
+def data_engineering():
+    return render_template('dataengineering.html')
+
+
 
 #-------------------- RUN --------------------
 if __name__ == "__main__":
